1ea2b3d19be0151ee4581ae59ec0b967b2600393
Joako <joako12409@gmail.com>
2024-04-05 15:58:39 -0300

    Login Github Finished.

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -2,24 +2,23 @@ import { getServerSession } from "next-auth";
 import Image from "next/image";
 import ButtonSignout from "./ButtonSignout";
 
-
-async function DashboardPage(){
-
- const session = await   getServerSession()
-console.log(session);
-    return(
-        <div className="flex justify-center items-center h-screen">
-           <div className="bg-slate-300 p-10 text-center">
-           <h1>Bienvenido {session?.user?.name}</h1>
-            <p>{session?.user?.email}</p>
-            <Image src={session?.user?.image || ""}
-             alt="image profile"
-             width={200}
-             height={200}
-            />
-            <ButtonSignout/>
-           </div>
-        </div>
-    )
+async function DashboardPage() {
+  const session = await getServerSession();
+  console.log(session);
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <div className="bg-slate-300 p-10 text-center">
+        <h1>Bienvenido {session?.user?.name}</h1>
+        <p>{session?.user?.email}</p>
+        <Image
+          src={session?.user?.image || ""}
+          alt="image profile"
+          width={200}
+          height={200}
+        />
+        <ButtonSignout />
+      </div>
+    </div>
+  );
 }
 export default DashboardPage;
