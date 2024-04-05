f63ec1122437140375a5440800cb4c14a7c28cf4
Joako <joako12409@gmail.com>
2024-04-05 14:26:25 -0300

    Signout Finished.

---
+++ src/app/dashboard/ButtonSignout.tsx
@@ -0,0 +1,15 @@
+"use client"
+import { signOut } from "next-auth/react";
+
+function ButtonSignout(){
+    return(
+        <button onClick={() =>{
+            signOut()
+        }}
+        className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded-full"
+        >
+            Cerrar Sesion
+        </button>
+    )
+}
+export default ButtonSignout;
